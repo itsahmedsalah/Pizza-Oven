af8ece59b4d5dceb5f503b01fdd351585f231f61
AhmedSalah <ahmedsaaalah21@gmail.com>
2025-06-12 10:13:30 +0300

    Add the fonts and the images for components

---
+++ app/src/main/java/com/example/pizzaoven/ui/theme/Fonts.kt
@@ -0,0 +1,12 @@
+package com.example.pizzaoven.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.example.pizzaoven.R
+
+val Poppins = FontFamily(
+    Font(resId = R.font.poppins_regular, weight = FontWeight.Normal),
+    Font(resId = R.font.poppins_medium, weight = FontWeight.Medium),
+    Font(resId = R.font.poppins_semi_bold, weight = FontWeight.SemiBold),
+)
